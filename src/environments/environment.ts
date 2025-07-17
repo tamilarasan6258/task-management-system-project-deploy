8d4bcd845313fe731b359e28363acb8b48288c46
tamilarasan6258 <sstamilarasan14@gmail.com>
2025-07-17 20:32:51 +0530

    fix : sign up page

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  auth_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/auth',
+  password_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/password',
+  project_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/projects',
+  task_apiBaseUrl: 'https://task-management-system-project-deploy.onrender.com/api/tasks'
+};
